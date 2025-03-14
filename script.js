abb4bd791f018fa81e0f6dadba56170ba4c211e4
Alisson Teles Fraga <alissontfraga@gmail.com>
2025-03-13 23:35:38 -0300

    Estrutura inicial

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("copiar").addEventListener("click", copiarEmail);
+
+function copiarEmail() {
+    const email = "alissontfraga@gmail.com";
+    navigator.clipboard.writeText(email).then(() => {
+        document.getElementById("mensagem").innerText = "E-mail copiado!";
+        setTimeout(() => {
+            document.getElementById("mensagem").innerText = "";
+        }, 2000);
+    }).catch(erro => {
+        console.error("Erro ao copiar o e-mail:", erro);
+    });
+}
